c6ed4e7eb9c65b77955e4b64a969da0fb447fd28
Riddhi Doshi <48765727+rdoshi29@users.noreply.github.com>
2019-12-22 13:36:16 +0530

    Update select.php

--- select.php
+++ select.php
@@ -1,34 +1,52 @@
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</head>
 <?php 
      session_start();
  if(isset($_POST["task_id"]))  
- {  
-      $output = '';  
+ {    
       include('connect.php'); 
       $hey = $_SESSION['username'];
       $query = "SELECT * FROM task WHERE username = '$hey' and taskid = '".$_POST["task_id"]."'";  
       $result = mysqli_query($conn, $query);  
-      $output .= '  
-      <div class="table-responsive">  
-            <table class="table table-bordered">';  
       while($row = mysqli_fetch_array($result))  
       {  
-           $output .= '  
-                <tr>  
-                     <td width="30%"><label>Task</label></td>  
-                     <td width="70%">'.$row["tname"].'</td>  
-                </tr>  
-                <tr>  
-                     <td width="30%"><label>Description</label></td>  
-                     <td width="70%">'.$row["detail"].'</td>  
-                </tr>  
-                <tr>  
-                     <td width="30%"><label>Deadline</label></td>  
-                     <td width="70%">'.$row["deadline"].'</td>  
-                </tr> 
-                ';  
+          $task_name = $row['tname'];
+          $deadline = $row['deadline'];
+          $detail = $row['detail'];
+          $taskno = $row['taskid'];
       }  
-      $output .= '</table><button type="submit" class="btn btn-primary" name="editbutton"><img src="https://cdn2.iconfinder.com/data/icons/apple-classic/100/Apple_classic_10Icon_5px_grid-04-512.png"></button> 
-      </div>';  
-      echo $output; 
-      
- }  
+  } 
+?>
+          <form action="index.php" method="post" style="width: 100%;">
+        <p style="margin-left: 15px; margin-right: 15px; margin-top: 0px; margin-bottom: 0px;">Task Name</p>
+        <div class="input-group">
+          <input type="text" style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 50px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" id="taskname" class="form-control validate" name="task" value="<?php echo $task_name; ?>">
+        </div>
+        <p style="margin-left: 15px; margin-right: 15px; margin-top: 0px; margin-bottom: 0px;">Deadline</p>
+        <div class="input-group">
+          <input type="date" style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 50px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" id="deadline" class="form-control validate" name="deadline" value="<?php echo $deadline; ?>">
+        </div>
+        <p style="margin-left: 15px; margin-right: 15px; margin-top: 0px; margin-bottom: 0px;">Description</p>
+        <div class="input-group">
+          <textarea style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 150px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" rows="3" id="detail" class="form-control validate" name="detail" ><?php echo $detail; ?></textarea>
+        </div>
+        <div class="input-group">
+        <input type="hidden" value="<?php echo $taskno; ?>" name="taskno">
+        </div>
+      </div>
+      <div class="modal-footer">
+      <div class="d-flex">
+        <button type="submit" name="save" class="btn btn-info"><img src="https://cdn2.iconfinder.com/data/icons/web-application-icons-part-2/100/Artboard_73-512.png"></button>
+      </div>
+        <button type="submit" class="btn btn-danger" name="deletebutton"><img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/delete-1432400-1211078.png"></button> 
+      </div>  
+
+    </div>
+    </form>
+
+
